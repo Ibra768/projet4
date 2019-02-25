d360ba64bbe84d5d28c500ad47c8608e9a9b5046
Ibrahim <i.benfaiala@gmail.com>
2019-02-25 11:57:38 +0100

    modification login mdp DB

--- model/Manager.php
+++ model/Manager.php
@@ -1,17 +1,4 @@
-<?php
 
-namespace Model;
-
-class Manager
-{
-    protected function dbConnect() // Permet la connexion a la base de données
-    {
-        $db = new \PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
-        return $db;
-    }
-}
-
-/*
 <?php
 
 namespace Model;
@@ -24,4 +11,3 @@ class Manager
         return $db;
     }
 }
-*/
